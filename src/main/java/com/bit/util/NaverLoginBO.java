599f64fc139f5b41f1a27f7aecfc3ddda0dbff56
afigurekim <afigurekim@gmail.com>
2019-03-18 16:46:04 +0900

    NaverLoginBO 수정 (ktrace.tk -> www.ktrace.tk)

--- src/main/java/com/bit/util/NaverLoginBO.java
+++ src/main/java/com/bit/util/NaverLoginBO.java
@@ -24,7 +24,7 @@ public class NaverLoginBO {
     //state: 애플리케이션이 생성한 상태 토큰
     private final static String CLIENT_ID = "hrGwx9O63YdZCfKyPw1D";       //네이버API Client ID
     private final static String CLIENT_SECRET = "XhMgrJefoV";                      
-    private final static String REDIRECT_URI = "http://ktrace.tk/callback";
+    private final static String REDIRECT_URI = "http://www.ktrace.tk/callback";
     private final static String SESSION_STATE = "oauth_state";
     /* 프로필 조회 API URL */
     private final static String PROFILE_API_URL = "https://openapi.naver.com/v1/nid/me";/// Api 종류 기본 !!
